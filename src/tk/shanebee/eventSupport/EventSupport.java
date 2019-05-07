fc4dbd3d6aefc3c44a13f4cd1a4a169b69823788
ShaneBeee <shanebolenback@me.com>
2019-05-07 02:59:24 -0700

    EventSupport - Changes - Code cleanup

--- src/tk/shanebee/eventSupport/EventSupport.java
+++ src/tk/shanebee/eventSupport/EventSupport.java
@@ -3,55 +3,37 @@ package tk.shanebee.eventSupport;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import tk.shanebee.eventSupport.events.*;
 import tk.shanebee.eventSupport.metrics.Metrics;
 
 public class EventSupport extends JavaPlugin {
-    private FileConfiguration config = this.getConfig();
 
+    private FileConfiguration config = this.getConfig();
+    private String version = ChatColor.GRAY + " v" + getDescription().getVersion();
+    private String name = getDescription().getName();
+    private String prefix = ChatColor.GRAY + "[" + ChatColor.AQUA + name + ChatColor.GRAY + "]";
 
     @Override
     public void onEnable() {
         Metrics metrics = new Metrics(this);
 
-        PluginDescriptionFile pdfFile = getDescription();
-        String prefix = ChatColor.GRAY + "[" + ChatColor.AQUA + pdfFile.getName() +
-                ChatColor.GRAY + "]";
-        String version = ChatColor.GRAY + " v" + pdfFile.getVersion();
-        String name = pdfFile.getName();
-
-        getServer().getConsoleSender().sendMessage(prefix + ChatColor.GREEN + " Enabled " + name + version);
+        sendConsoleMessage(prefix + "&2 Enabled " + name + version);
         if(!isRunningPaper()) {
-            getServer().getConsoleSender().sendMessage(prefix + ChatColor.LIGHT_PURPLE + " If you run Paper, more events will be available");
+            sendConsoleMessage(prefix + "&d If you run Paper, more events will be available");
         }
-
         Configuration.loadConfig(config);
         saveConfig();
-
-        getServer().getPluginManager().registerEvents(new PlayerEvents(this), this);
-        getServer().getPluginManager().registerEvents(new EntityEvents(this), this);
-        getServer().getPluginManager().registerEvents(new BlockEvents(this), this);
-        getServer().getPluginManager().registerEvents(new ServerEvents(this), this);
-        if(isRunningPaper()) {
-            getServer().getPluginManager().registerEvents(new PaperEvents(this), this);
-        }
-        this.getCommand("eventhandler").setExecutor(new Commands(this));
-        this.getCommand("eventhandler").setTabCompleter(new TabCompleter(this));
+        registerEvents();
     }
 
     @Override
     public void onDisable() {
-        PluginDescriptionFile pdfFile = getDescription();
-        String PluginNameAndVersion = ChatColor.DARK_GRAY + "[" + ChatColor.AQUA + pdfFile.getName() +
-                ChatColor.DARK_GRAY + "]" + ChatColor.BLUE + " Disabled " + pdfFile.getName() + ChatColor.GRAY + " v" + pdfFile.getVersion()
-                + ChatColor.RESET;
-        getServer().getConsoleSender().sendMessage(PluginNameAndVersion);
+        sendConsoleMessage(prefix + "&1 Disabled " + getDescription().getName() + version);
     }
 
     /**
-     * Checks if this server is running an instance of Paper or not
+     * Checks if this server is running an instance of Paper
      * @return True if server is running Paper
      */
     static boolean isRunningPaper() {
@@ -61,6 +43,22 @@ public class EventSupport extends JavaPlugin {
         } catch (final ClassNotFoundException e) {
             return false;
         }
+    }
+
+    private void sendConsoleMessage(String message) {
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+    }
+
+    private void registerEvents() {
+        getServer().getPluginManager().registerEvents(new PlayerEvents(this), this);
+        getServer().getPluginManager().registerEvents(new EntityEvents(this), this);
+        getServer().getPluginManager().registerEvents(new BlockEvents(this), this);
+        getServer().getPluginManager().registerEvents(new ServerEvents(this), this);
+        if(isRunningPaper()) {
+            getServer().getPluginManager().registerEvents(new PaperEvents(this), this);
+        }
+        this.getCommand("eventhandler").setExecutor(new Commands(this));
+        this.getCommand("eventhandler").setTabCompleter(new TabCompleter(this));
     }
 
     public boolean hasWorld(String path, World world) {
